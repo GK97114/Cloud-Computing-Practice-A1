02c0196961c37013f48cda20c6711197e5fec792
Grant Kostrzewa <grantkoz11@gmail.com>
2026-03-29 22:59:50 -0500

    refactor: Controller is now async with proper Postgre syntax

--- controller/productController.js
+++ controller/productController.js
@@ -1,3 +1,4 @@
+const pool = require("../db/database");
 
 /**
  * Gets all products from the database
@@ -7,8 +8,14 @@
  * @param {import("express").Request} req is the request object used to access query parameters, body data, etc.
  * @param {import("express").Response} res is the response object used to send back the list of products or an error message
  */
-const getAllProducts = (req, res) => {
-    
+const getAllProducts = async (req, res) => {
+    try {
+        const result = await db.query("SELECT * FROM products");
+        res.json(result.rows);
+    } catch (error) {
+        console.error("Error fetching products:", error);
+        res.status(500).json({ error: "Failed to fetch products" });
+    }
 };
 
 /**
@@ -20,9 +27,9 @@ const getAllProducts = (req, res) => {
  * @param {import("express").Request} req is the request object containing the product data in req.body
  * @param {import("express").Response} res is the response object used to send back the created product or an error message
  */
-const createProduct = (req, res) => {
+const createProduct = async (req, res) => {
     try {
-        console.log("createProduct hit", req.body);
+        // console.log("createProduct hit", req.body);
 
         const { name, price } = req.body;
 
@@ -35,11 +42,13 @@ const createProduct = (req, res) => {
         }
 
         // Prepare and execute the insert statement
-
-        // Return the newly created product with its ID
+        const result = await db.query(
+            "INSERT INTO products (name, price) VALUES ($1, $2) RETURNING id, name, price",
+            [name, price]
+        );
 
         // Return the new product as JSON
-        res.status(201).json(newProduct);
+        res.status(201).json(result.rows[0]);
 
     } catch (error) {
         console.error("Error creating product:", error);
